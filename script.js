73a88857da372df3029641798c8d613b86ddbad3
John Schwartz <johnvincentschwartz@gmail.com>
2020-11-21 12:49:17 -0600

    bonus

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 window.addEventListener("load", function() {
 
+   const index = Math.floor(Math.random() * 6)
+
    fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
       response.json().then(function(json) {
          const missionTarget = document.getElementById("missionTarget");
          missionTarget.innerHTML = `
             <h2>Mission Destination</h2>
             <ol>
-               <li>Name: ${json[0].name}</li>
-               <li>Diameter: ${json[0].diameter}</li>
-               <li>Star: ${json[0].star}</li>
-               <li>Distance from Earth: ${json[0].distance}</li>
-               <li>Number of Moons: ${json[0].moons}</li>
+               <li>Name: ${json[index].name}</li>
+               <li>Diameter: ${json[index].diameter}</li>
+               <li>Star: ${json[index].star}</li>
+               <li>Distance from Earth: ${json[index].distance}</li>
+               <li>Number of Moons: ${json[index].moons}</li>
             </ol>
-            <img src="${json[0].image}">
+            <img src="${json[index].image}">
          `;
       });
    });
